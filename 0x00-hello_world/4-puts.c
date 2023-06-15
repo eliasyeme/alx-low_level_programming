9d1d156be1f145ba84edee93e0fa6089f4abe414
eliasyeme <eliasyeme@gmail.com>
2023-06-15 11:11:08 +0300

    add c program that prints specifiec message to console

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * print to stdout
+  * Return: 0 on success
+  */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
