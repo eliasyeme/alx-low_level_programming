016a6be808f2e5ac47c8c563d9356332579b50c3
eliasyeme <eliasyeme@gmail.com>
2023-06-15 11:28:30 +0300

    add c program to print specfiec message to stdout using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* print to stdout using printf
+* Return: 0 on success
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
