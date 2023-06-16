6d185493614f1afc3b11e34726433cfbe4d33981
eliasyeme <eliasyeme@gmail.com>
2023-06-16 14:30:33 +0300

    c program to print all possible three combination of 0-9

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,19 +7,24 @@ int main(void)
 {
 	int a;
 	int b;
+	int c;
 
 	for (a = '0'; a <= '9'; a++)
 	{
 		for (b = a; b <= '9'; b++)
 		{
-			if (a != b)
+			for (c = b; c <= '9'; c++)
 			{
-				putchar(a);
-				putchar(b);
-				if (a + b < '8' + '9')
+				if (a != b && b != c)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a + b + c < '7' + '8' + '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
