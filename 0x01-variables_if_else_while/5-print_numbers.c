0019d4c10d551f0cc3443cd74b3ac5e4b9706176
eliasyeme <eliasyeme@gmail.com>
2023-06-16 11:42:28 +0300

    add c program to print 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Return: 0 on success
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
