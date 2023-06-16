a8467d917667a14bc934ad385fe7c41fb7098179
eliasyeme <eliasyeme@gmail.com>
2023-06-16 11:52:34 +0300

    add c program to print 0-9 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Return: 0 on success
+  */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
