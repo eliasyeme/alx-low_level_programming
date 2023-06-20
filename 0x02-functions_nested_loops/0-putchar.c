2818f3fbdf7e37863dd51df145bffae887781f80
eliasyeme <eliasyeme@gmail.com>
2023-06-20 09:26:37 +0300

    include header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
  * Return: 0 on success
  */
-
 int main(void)
 {
-	printf("_putchar\n");
+	char *word = "_putchar\0";
+	int count = 0;
+	char first_letter = word[count];
+
+	while (first_letter != '\0')
+	{
+		_putchar(first_letter);
+		count++;
+		first_letter = word[count];
+	}
+	_putchar('\n');
 	return (0);
 }
