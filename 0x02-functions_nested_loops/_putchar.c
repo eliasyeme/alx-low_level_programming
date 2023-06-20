bc229c2b1372cfb4a175ee55663822c8e54bc923
eliasyeme <eliasyeme@gmail.com>
2023-06-20 10:08:37 +0300

    trvila

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+  * _putchar - write character c to stdout
+  * @c: Character to print
+  * Return: 1 on success
+  */
+int _putchar(char c)
+{
+	 write(STDIN_FILENO, &c, 1);
+   return 1;
+}
