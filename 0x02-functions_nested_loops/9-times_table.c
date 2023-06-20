5c0b5ac352cb16ac34ac80780df28457bc0383eb
eliasyeme <eliasyeme@gmail.com>
2023-06-20 19:27:03 +0300

    remove last comma

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,13 +19,15 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(mult + '0');
-				_putchar(',');
 			}
 			else
 			{
 				_putchar(' ');
 				_putchar((mult / 10) + '0');
 				_putchar((mult % 10) + '0');
+			}
+			if (y != 9)
+			{
 				_putchar(',');
 			}
 		}
