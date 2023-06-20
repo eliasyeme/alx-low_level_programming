953963964e3e7cb2521cea4692c286d3b8a48bc3
eliasyeme <eliasyeme@gmail.com>
2023-06-20 19:16:23 +0300

    testing hypotisisisisi

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,16 @@
  */
 int print_last_digit(int n)
 {
+
+	n %= 10;
 	if (n < 0)
 	{
 		n *= -1;
+		_putchar(n + '0');
 	}
-	n %= 10;
-	_putchar(n + '0');
+	else
+	{
+		_putchar(n + '0');
+	}
 	return (n);
 }
