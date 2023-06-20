71d81758f4e2a776685d73581f88048eec265217
eliasyeme <eliasyeme@gmail.com>
2023-06-20 10:07:18 +0300

    print the alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
