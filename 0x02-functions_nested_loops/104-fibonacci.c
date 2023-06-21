926e413c9239988021831b09ed208d07c566befb
eliasyeme <eliasyeme@gmail.com>
2023-06-21 15:50:52 +0300

    why twice

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - first 50 fibonacci
+ * Return: 0 on sucess
+ */
+int main(void)
+{
+	unsigned long num1 = 0;
+	unsigned long num2 = 1;
+	unsigned long next;
+	int i = 0;
+
+	for (; i < 99; i++)
+	{
+		next = num1 + num2;
+		num1 = num2;
+		num2 = next;
+		printf("%lu", next);
+		if (i != 98)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
