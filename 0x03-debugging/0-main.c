7e771ec0576bc084baf3c0a79ca2281a7e4ac7cc
eliasyeme <eliasyeme@gmail.com>
2023-06-21 07:05:41 +0300

    check case 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,15 +2,11 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- * Return: 0
+ * Return: 0 always
  */
 
 int main(void)
 {
-	int i;
-
-	i = 98;
-	positive_or_negative(i);
-
+	positive_or_negative(0);
 	return (0);
 }
