11d377107fe86a327a42109fac6f67a9de62e680
eliasyeme <eliasyeme@gmail.com>
2023-06-21 04:24:37 +0300

    add if a number is a multiple of 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * sum_of_3_or_5 - print the sum of all multiples of 3 or 5
+ */
+void sum_multiples_of_3_or_5(void)
+{
+	int sum = 0;
+	int i = 0;
+
+	for (; i < 1024; i++)
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+
+	printf("%d\n", sum);
+}
