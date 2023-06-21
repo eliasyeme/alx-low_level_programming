5e98a56793905c79d4101bc90d9bda3296b98644
eliasyeme <eliasyeme@gmail.com>
2023-06-21 05:17:40 +0300

    the tests are in consistant

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,16 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * sum_of_3_or_5 - print the sum of all multiples of 3 or 5
+ * Return: 0 on sucess
  */
-void sum_multiples_of_3_or_5(void)
+int main(void)
 {
 	int sum = 0;
 	int i = 0;
 
 	for (; i < 1024; i++)
-		if (i % 3 == 0 || i % 5 == 0)
+		if ((i % 3 == 0) || (i % 5 == 0))
 			sum += i;
 
 	printf("%d\n", sum);
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,5 +11,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void sum_multiples_of_3_or_5(void);
 void print_times_table(int n);
