aee1f833fed6f95681c30642044f674342efcbf6
eliasyeme <eliasyeme@gmail.com>
2023-06-22 14:28:05 +0300

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_triangle - print right angle triangle
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = size - i; j > 0; j--)
+			_putchar(' ');
+
+		for (k = 0; k <= i; k++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
