41af025ab941d9f928050edbdad78df4c6dbe572
eliasyeme <eliasyeme@gmail.com>
2023-06-22 13:26:38 +0300

    squares of #

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_square - print a swuare of #
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
