65a544939d30c207604155b301009b676ddcc909
eliasyeme <eliasyeme@gmail.com>
2023-06-26 09:29:35 +0300

    reverse a string in place

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int i = 0;
+	char tmp;
+
+	while (s[len] != '\0')
+		len++;
+
+	for (; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
+	}
+}
