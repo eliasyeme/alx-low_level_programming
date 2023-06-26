322f13d801569a09fbca25b40fd7d33a56609d89
eliasyeme <eliasyeme@gmail.com>
2023-06-26 10:02:42 +0300

    print elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_array - print specified elents of an array
+ * @a: input array
+ * @n: number of element
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+		printf("%d, ", a[i]);
+
+	printf("%d\n", a[n - 1]);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdio.h>
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
