3401c71e360816f962ea2999a921729b2d3f6154
eliasyeme <eliasyeme@gmail.com>
2023-06-26 12:30:02 +0300

    no negative

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,8 +9,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n - 1; i++)
-		printf("%d, ", a[i]);
+	if (n >= 0)
+	{
+		for (i = 0; i < n - 1; i++)
+			printf("%d, ", a[i]);
 
-	printf("%d\n", a[n - 1]);
+		printf("%d\n", a[n - 1]);
+	}
 }
