99cbcafc74bf05a8e3fefaa1530a9e27c5581fc9
eliasyeme <eliasyeme@gmail.com>
2023-06-26 10:14:27 +0300

    scuffed strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+/**
+ * _strcpy - copy string from src to dest
+ * @dest: destination for the string to be copied
+ * @src: input string to copy
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, len = 0;
+
+	while (src[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
