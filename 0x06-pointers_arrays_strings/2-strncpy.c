96c3903d6d31c40cbf670d31025134c3491d0ed0
eliasyeme <eliasyeme@gmail.com>
2023-06-29 11:44:09 +0300

    made scuffed strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * _strncpy - copy string
+ * @dest: destination
+ * @src: source
+ * @n: restriction
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
