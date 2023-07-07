a8dd4b2fb1875760867cc04c930855feb2116e7c
eliasyeme <eliasyeme@gmail.com>
2023-07-07 11:18:38 +0300

    print all arguments passed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print arguments passed
+ * @argc: argument count
+ * @argv: argument array
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
