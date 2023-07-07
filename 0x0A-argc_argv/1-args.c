be016232fea5827efd6514be75e60f8f7f772829
eliasyeme <eliasyeme@gmail.com>
2023-07-07 11:10:19 +0300

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * @argc: argument count
+ * @argv: argument array
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char __attribute__((unused)) **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
