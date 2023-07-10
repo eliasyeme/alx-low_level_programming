6b9aaf88b65704f1182ee28d257b070a4989f923
eliasyeme <eliasyeme@gmail.com>
2023-07-10 16:07:20 +0300

    free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,1 +1,16 @@
-void free_grid(int **grid, int height);
+#include "main.h"
+
+/**
+	* free_grid - free 2D array
+	* @grid: input memory
+	* @height: height of input memory
+	*/
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
