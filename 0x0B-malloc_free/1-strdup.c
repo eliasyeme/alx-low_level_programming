6d5c3e027f8bc1b97c7119ec15238ed94f1aea94
eliasyeme <eliasyeme@gmail.com>
2023-07-10 09:44:12 +0300

    duplicate string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,1 +1,36 @@
-char *_strdup(char *str);
+#include "main.h"
+
+/**
+	* _strdup - duplicate string
+	* @str: input string
+	*
+	* Return: pointer to duplicated string
+	*/
+char *_strdup(char *str)
+{
+	int i = 0;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i])
+		i++;
+
+	s = (char *) malloc(sizeof(char) * (i + 1));
+
+	if (s != NULL)
+	{
+		for (i = 0; str[i]; i++)
+		{
+			s[i] = str[i];
+		}
+		s[i + 1] = '\0';
+	}
+	else
+	{
+		return (NULL);
+	}
+
+	return (s);
+}
