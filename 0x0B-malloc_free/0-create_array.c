57145eae6a38d2bff5ab1b62bffb15c0a898a8e8
eliasyeme <eliasyeme@gmail.com>
2023-07-10 09:00:13 +0300

    setup

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,10 @@
+/**
+	* create_array - create array and initialize
+	* with a specfic char
+	* @size: size of the array
+	* @c: character to initialize with
+	*
+	* Return: pointer to initialized array or
+	* NULL if size is 0
+	*/
+char *create_array(unsigned int size, char c);
