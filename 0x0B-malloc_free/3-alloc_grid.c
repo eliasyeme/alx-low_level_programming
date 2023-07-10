49ba5ecad01a7ef88a5d086d79f0aea3cac88fff
eliasyeme <eliasyeme@gmail.com>
2023-07-10 15:35:07 +0300

    rollback

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,9 +27,9 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 	}
 
-	for (i = 0; i < height; i++)
+	for (i = 0; i < width; i++)
 	{
-		for (j = 0; j < width; j++)
+		for (j = 0; j < height; j++)
 		{
 			p[i][j] = 0;
 		}
