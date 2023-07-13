3903ab53a08ca8661ad0d91ead3aa0e3eeb01b1a
eliasyeme <eliasyeme@gmail.com>
2023-07-13 10:20:40 +0300

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,1 +1,31 @@
-void *_calloc(unsigned int nmemb, unsigned int size);
+#include "main.h"
+
+/**
+	* _calloc - allocate memory for an array using malloc
+	* @nmemb: number of element
+	* @size: byte size
+	*
+	* Return: pointer to allocated memory or NULL if either nmemb
+	* or size is 0 and if malloc fails
+	*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	unsigned char *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
+
+	return (p);
+}
