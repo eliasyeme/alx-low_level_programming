79d7b616a6d9ea0e7fe85c98cc2e7bd42f4d236c
eliasyeme <eliasyeme@gmail.com>
2023-07-13 13:08:30 +0300

    stringncat test4

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n < len2)
 		len2 = n;
 
-	s = malloc(sizeof(*s) * (len1 + len2));
+	s = malloc(sizeof(*s) * (len1 + len2 + 1));
 
 	if (s == NULL)
 	{
