87ceb4fb95e82035019da0c905375fa9585fc33d
eliasyeme <eliasyeme@gmail.com>
2023-07-13 10:49:27 +0300

    number range array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,1 +1,27 @@
-int *array_range(int min, int max);
+#include "main.h"
+
+/**
+	* array_range - create array of integers
+	* @min: from
+	* @max: to
+	*
+	* Return: pointer to array
+	*/
+int *array_range(int min, int max)
+{
+	int i, j;
+	int *p;
+
+	if (min > max)
+		return (NULL);
+
+	p = malloc(sizeof(*p) * ((max - min) + 1));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = min, j = 0; i <= max; i++, j++)
+		p[j] = i;
+
+	return (p);
+}
