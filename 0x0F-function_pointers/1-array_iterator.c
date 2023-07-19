b01e7956e68d5e8fff3961d32cdd7b6f3d62e69a
eliasyeme <eliasyeme@gmail.com>
2023-07-19 09:09:11 +0300

    callback fun

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,2 +1,15 @@
 #include "function_pointers.h"
-void array_iterator(int *array, size_t size, void (*action)(int));
+
+/**
+	* array_iterator - map over an element in array
+	* @array: array to map over
+	* @size: size of the array
+	* @action: callback function
+	*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
