2ed5700ebbce3afb6ce4a109749cd94ddbe0284e
eliasyeme <eliasyeme@gmail.com>
2023-07-19 10:37:59 +0300

    thanks but no thanks

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - perform arithmetic operations
+ * @argc: argument count
+ * @argv: argument value
+ *
+ * Return: EXIT_SUCCESS on success
+ */
+int main(int argc, char **argv)
+{
+	int (*res)(int, int);
+	int a, b;
+	char opp;
+
+	if (argc < 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	opp = *argv[2];
+
+	res = get_op_func(argv[2]);
+
+	if (res == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((b == 0) && (opp == '/' || opp == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", res(a, b));
+
+	return (EXIT_SUCCESS);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * op_add - sum of two numbers
+ * @a: input number 1
+ * @b: input number 2
+ * Return: the sum of two numbers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference of two numbers
+ * @a: input number 1
+ * @b: input number 2
+ * Return: the difference of two numbers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product of two numbers
+ * @a: input number 1
+ * @b: input number 2
+ * Return: the product of two numbers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of two numbers
+ * @a: input number 1
+ * @b: input number 2
+ * Return: the division of two numbers
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder of the division of two numbers
+ * @a: input number 1
+ * @b: input number 2
+ * Return: the remainder of the division of two numbers
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
