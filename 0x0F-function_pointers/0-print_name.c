6ae5df12f175aad2a72ff40fcc3faeda8ecde524
eliasyeme <eliasyeme@gmail.com>
2023-07-19 09:03:36 +0300

    ok

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,9 @@
-void print_name(char *name, void (*f)(char *));
+/**
+	* print_name - prints string using callback function
+	* @name: name string
+	* @f: callback function
+	*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
