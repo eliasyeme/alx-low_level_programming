59b0208fc392bcddcfcdc0749f3ca6ac821a5a18
eliasyeme <eliasyeme@gmail.com>
2023-07-20 12:31:56 +0300

    test

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,9 +11,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	if (n == 0)
-		return;
-
 	va_start(args, n);
 	if (separator != NULL)
 	{
