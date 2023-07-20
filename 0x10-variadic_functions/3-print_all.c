76e63580c25fd7165bc0e9dfa2571964236ce129
eliasyeme <eliasyeme@gmail.com>
2023-07-20 11:55:27 +0300

    alx really really loves printing

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,1 +1,45 @@
-void print_all(const char * const format, ...);
+#include "variadic_functions.h"
+/**
+ * print_all - print according to format specifier
+ * @format: format specifier
+ */
+void print_all(const char * const format, ...)
+{
+	int i, j;
+	char *s;
+	va_list args;
+
+	i = 0;
+	va_start(args, format);
+	while (format && format[i])
+	{
+		j = 0;
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%i", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (!s)
+					s = "(nil)";
+				printf("%s", s);
+				break;
+			default:
+				j = 1;
+				break;
+		}
+		if (format[i + 1] && !j)
+			printf(", ");
+		i++;
+	}
+	va_end(args);
+
+	printf("\n");
+}
