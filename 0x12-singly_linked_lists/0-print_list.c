2d760247b65af78935c7aa90b1803a5e0a8cc39e
eliasyeme <eliasyeme@gmail.com>
2023-07-27 12:22:45 +0300

    req fail test3

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,17 +8,17 @@
  */
 size_t print_list(const list_t *h)
 {
+	const list_t *head = h;
 	size_t i = 0;
 
-	while (h)
+	while (head)
 	{
-		if (!h->str)
-			printf("[0] (nil)\n");
+		if (head->str)
+			printf("[%d] %s\n", head->len, head->str);
 		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		h = h->next;
+			printf("[0] (nil)\n");
 		i++;
+		head = head->next;
 	}
 
 	return (i);
