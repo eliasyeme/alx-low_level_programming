916723c03a18f8380f5d1b7f127deb097c575ab7
eliasyeme <eliasyeme@gmail.com>
2023-07-27 09:39:14 +0300

    print lls

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,30 @@
 #include "lists.h"
 
 /**
+ * print_list - print len and string
+ * @h: input list
  *
+ * Return: number of elements printed
  */
 size_t print_list(const list_t *h)
 {
+	size_t i = 0;
+	int len;
+	char *s;
 
+	while (h)
+	{
+		s = h->str;
+		len = h->len;
+		if (!s)
+		{
+			s = "(nil)";
+			len = 0;
+		}
+		printf("[%d] %s\n", len, s);
+		h = h->next;
+		i++;
+	}
+
+	return (i);
 }
