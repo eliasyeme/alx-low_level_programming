0e4c905de44d6fdfe180088d6ec3bd8b221a8cac
eliasyeme <eliasyeme@gmail.com>
2023-07-27 09:48:40 +0300

    counting list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,9 +1,19 @@
 #include "lists.h"
 
 /**
- *
+ * list_len - length of a linked list
+ * @h: input list
+ * Return: number of elements
  */
 size_t list_len(const list_t *h)
 {
+	size_t len = 0;
 
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+
+	return (len);
 }
