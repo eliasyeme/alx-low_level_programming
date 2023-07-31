e0130e3d23767c5be14e492f3c4bb0d203d6a4de
eliasyeme <eliasyeme@gmail.com>
2023-07-31 19:28:07 +0300

    Sum elements of a list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum data of a list
+ * @head: input list
+ *
+ * Return: sum of the data 0 if no data
+ */
+int sum_listint(listint_t *head)
+{
+  int i = 0;
+
+	while (head)
+	{
+		i += head->n;
+		head = head->next;
+	}
+
+  return (i);
+}
