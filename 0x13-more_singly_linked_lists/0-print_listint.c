3fc52119caf57cb924d437b626e7109150989d64
eliasyeme <eliasyeme@gmail.com>
2023-07-31 07:00:07 +0300

    Add print linked list and return length

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,4 +6,16 @@
 	*
 	* Return: number of nodes
 	*/
-size_t print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		len++;
+	}
+
+	return (len);
+}
