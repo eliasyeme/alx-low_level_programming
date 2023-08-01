1ca6944c1c661fe5aced5394a38e460637b8463b
eliasyeme <eliasyeme@gmail.com>
2023-08-01 10:05:07 +0300

    Experment separating some conditionals 3

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -44,8 +44,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new_node->next = node->next;
 		node->next = new_node;
-		return (new_node);
 	}
-	free(new_node);
-	return (NULL);
+
+	return (new_node);
 }
