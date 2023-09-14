7b29f47a1a557c36d1f444f3a4476411933b0169
eliasyeme <eliasyeme@gmail.com>
2023-09-14 14:04:51 +0300

    free

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -4,4 +4,14 @@
 	* free_dlistint - free dlistint_t list
 	* @head: input list
 	*/
-void free_dlistint(dlistint_t *head);
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
