a2bd0f44c1e3df943155bba4fe442773af4b6e86
eliasyeme <eliasyeme@gmail.com>
2023-09-14 14:13:12 +0300

    sum

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -7,4 +7,14 @@
 	* Return: sum of all data on success
 	*					0 if list is empty
 	*/
-int sum_dlistint(dlistint_t *head);
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
