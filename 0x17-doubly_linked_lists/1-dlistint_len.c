8d6609ed316eec5278b8f6a6c786d156c3923e6a
eliasyeme <eliasyeme@gmail.com>
2023-09-14 12:59:55 +0300

    len

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -6,4 +6,18 @@
 	*
 	* Return: number of elements
 	*/
-size_t dlistint_len(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len = 0;
+
+	if (!h)
+		return (len);
+
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+
+	return (len);
+}
