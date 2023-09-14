85488fd8f06aa5461800c8dab272692b5e053b88
eliasyeme <eliasyeme@gmail.com>
2023-09-14 19:26:48 +0300

    delete

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -8,4 +8,35 @@
 	* Return: 1 on success
 	*					-1 on fail
 	*/
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *node;
+	unsigned int i;
+
+	if (!*head)
+		return (-1);
+
+	node = *head;
+	if (index == 0)
+	{
+		*head = node->next;
+		if (node->next)
+			node->next->prev = NULL;
+		free(node);
+		return (1);
+	}
+
+	for (i = 0; i < index; i++)
+	{
+		if (!node->next)
+			return (-1);
+		node = node->next;
+	}
+
+	node->prev->next = node->next;
+	if (node->next)
+		node->next->prev = node->prev;
+	free(node);
+
+	return (1);
+}
