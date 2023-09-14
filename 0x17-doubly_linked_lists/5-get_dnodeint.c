b2f7e2bb5f81352d72b915ba492385544f18f321
eliasyeme <eliasyeme@gmail.com>
2023-09-14 14:10:51 +0300

    get nth

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -8,4 +8,20 @@
 	* Return:	pointer to the new element on success
 	*					NULL on fail
 	*/
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+
+	unsigned int i = 0;
+
+	if (index == 0)
+		return (head);
+
+	while (i < index)
+	{
+		if (head->next == NULL)
+			return (NULL);
+		head = head->next;
+		i++;
+	}
+	return (head);
+}
