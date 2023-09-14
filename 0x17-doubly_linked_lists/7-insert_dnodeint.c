e3c862fdbba118a4e080bb3a9d6a7dcc4e88fd6c
eliasyeme <eliasyeme@gmail.com>
2023-09-14 18:25:38 +0300

    insert at

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,28 @@
 #include "lists.h"
 
 /**
+	* len - number of elements in dlistint_t list
+	* @h: input list
+	*
+	* Return: number of elements
+	*/
+unsigned int len(const dlistint_t *h)
+{
+	unsigned int len = 0;
+
+	if (!h)
+		return (len);
+
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+
+	return (len);
+}
+
+/**
 	* insert_dnodeint_at_index - insert new node at a given index
 	* @h: input dlistint_t list
 	* @idx: position to insert at
@@ -9,4 +31,35 @@
 	* Return:	pointer to the new element on success
 	*					NULL on fail
 	*/
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new;
+	unsigned int i;
+
+	if (idx > len(*h))
+		return (NULL);
+
+
+	if (idx == 0)
+		new = add_dnodeint(h, n);
+	else
+	{
+		new = malloc(sizeof(dlistint_t));
+		if (!new)
+			return (NULL);
+
+		new->n = n;
+
+		for (i = 0; i < idx - 1; i++)
+			*h = (*h)->next;
+
+		new->prev = *h;
+		new->next = (*h)->next;
+		if ((*h)->next)
+			(*h)->next->prev = new;
+		(*h)->next = new;
+		while ((*h)->prev)
+			*h = (*h)->prev;
+	}
+	return (new);
+}
