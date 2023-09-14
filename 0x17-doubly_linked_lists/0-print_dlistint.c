2431671091f1baaacb014f7e1428df397faca05b
eliasyeme <eliasyeme@gmail.com>
2023-09-14 12:56:46 +0300

    print

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,4 +6,19 @@
  *
  * Return: number of nodes
  */
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t len = 0;
+
+	if (!h)
+		return (len);
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		len++;
+	}
+
+	return (len);
+}
