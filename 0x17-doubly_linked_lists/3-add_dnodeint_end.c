3e88054907b9402aec76a8be52661b153015faae
eliasyeme <eliasyeme@gmail.com>
2023-09-14 13:57:12 +0300

    append

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,4 +8,31 @@
 	* Return:	pointer to the new element on success
 	*					NULL on fail
 	*/
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (!*head)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+
+	while ((*head)->next)
+		*head = (*head)->next;
+	(*head)->next = new;
+	new->prev = *head;
+
+	while ((*head)->prev)
+		*head = (*head)->prev;
+
+	return (new);
+}
