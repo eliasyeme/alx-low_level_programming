6d423f6fa2975a487644731ab790a363edc27130
eliasyeme <eliasyeme@gmail.com>
2023-09-14 13:33:51 +0300

    prepend

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,4 +8,22 @@
 	* Return:	pointer to the new element on success
 	*					NULL on fail
 	*/
-dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+
+	if (*head)
+		(*head)->prev = new;
+
+	*head = new;
+
+	return (new);
+}
