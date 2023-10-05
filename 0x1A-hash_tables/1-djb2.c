7a7bf2e6332dcf93d0aa66fc49647bc3b8bf1ee2
eliasyeme <eliasyeme@gmail.com>
2023-10-05 13:38:51 +0300

    hash

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,9 +1,19 @@
 #include "hash_tables.h"
 
 /**
-	* hash_table_create - 
-	* @size: 
+	* hash_table_create - hash function using djb2
+	* @str: string to hash
 	*
-	* Return: 
+	* Return: hash number
 	*/
-unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c;
+
+	return (hash);
+}
