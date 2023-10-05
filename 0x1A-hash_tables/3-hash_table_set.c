2fbf2bd1c00da5b72bae66be693a86c7dd501064
eliasyeme <eliasyeme@gmail.com>
2023-10-05 16:12:09 +0300

    add

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,9 +1,79 @@
 #include "hash_tables.h"
 
 /**
-	* hash_table_create - 
-	* @size: 
+	* new_hash - create new node with key/value pair
+	* @key: the key
+	* @value: the value
 	*
-	* Return: 
+	* Return: pointer to new node or NULL
 	*/
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+hash_node_t *new_hash(const char *key, const char *value)
+{
+
+	hash_node_t *new;
+
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (NULL);
+
+	new->value = strdup(value);
+	if (!new->value)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->key = strdup(key);
+	if (!new->key)
+	{
+		free(new->value);
+		free(new);
+		return (NULL);
+	}
+
+	new->next = NULL;
+	return (new);
+}
+
+/**
+	* hash_table_set - add element to the hash table
+	* @ht: hash table to add/update the key/value to
+	* @key: the key which can't be empty string
+	* @value: value associated with the key
+	*
+	* Return: 1 on success
+	*					0 otherwise
+	*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new, *tmp;
+	unsigned long idx;
+	char *tmp_value;
+
+	if (!ht || !key || !value)
+		return (0);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[idx];
+
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			tmp_value = strdup(value);
+			if (!tmp_value)
+				return (0);
+			free(tmp->value);
+			tmp->value = tmp_value;
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+
+	new = new_hash(key, value);
+	if (!new)
+		return (0);
+
+	ht->array[idx] = new;
+	return (1);
+}
