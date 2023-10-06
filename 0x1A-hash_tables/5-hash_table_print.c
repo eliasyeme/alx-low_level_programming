6c55394420b6be062dd235593614bcc66ea958cb
eliasyeme <eliasyeme@gmail.com>
2023-10-06 09:55:19 +0300

    print

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,9 +1,33 @@
 #include "hash_tables.h"
 
 /**
-	* hash_table_create - 
-	* @size: 
-	*
-	* Return: 
+	* hash_table_print - print hash table
+	* @ht: hash table to print
 	*/
-void hash_table_print(const hash_table_t *ht);
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long i;
+	hash_node_t *tmp;
+	unsigned int flag = 0;
+
+	if (!ht)
+		return;
+
+	if (!ht->array)
+		printf("{}\n");
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp)
+		{
+			if (flag)
+				printf(", ");
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			flag = 1;
+			tmp = tmp->next;
+		}
+	}
+	printf("}\n");
+}
