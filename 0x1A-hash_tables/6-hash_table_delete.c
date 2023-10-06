87fd1697d2714d2468bef9b248ae7f0e342345d5
eliasyeme <eliasyeme@gmail.com>
2023-10-06 10:05:40 +0300

    delete

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,9 +1,33 @@
 #include "hash_tables.h"
 
 /**
-	* hash_table_create - 
-	* @size: 
-	*
-	* Return: 
+	* hash_table_delete - delete hash table
+	* @ht: the hash table
 	*/
-void hash_table_delete(hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp;
+	unsigned long i;
+
+	if (!ht)
+		return;
+
+	if (!ht->array)
+	{
+		free(ht);
+		return;
+	}
+
+	for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i])
+		{
+			tmp = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = tmp;
+		}
+	}
+	free(ht);
+}
