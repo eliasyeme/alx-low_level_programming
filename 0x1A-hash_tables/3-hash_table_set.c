a3d02429abaff57828428de7d8dafca74532a236
eliasyeme <eliasyeme@gmail.com>
2023-10-06 08:51:53 +0300

    test2

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,24 +36,6 @@ hash_node_t *new_hash(const char *key, const char *value)
 }
 
 /**
- * append - append to a linked list
- * @list: list to append to
- * @node: node to append
- */
-void append(hash_node_t *list, hash_node_t *node)
-{
-	while (1)
-	{
-		if (list->next == NULL)
-		{
-			list->next = node;
-			break;
-		}
-		list = list->next;
-	}
-}
-
-/**
 	* hash_table_set - add element to the hash table
 	* @ht: hash table to add/update the key/value to
 	* @key: the key which can't be empty string
@@ -92,14 +74,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!new)
 		return (0);
 
-	if (!ht->array[idx])
-	{
-		ht->array[idx] = new;
-		return (1);
-	}
-
-	tmp = ht->array[idx];
-	append(tmp, new);
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
 
 	return (1);
 }
