7227e47e3f84f074bc42715cb391bf9595a24a38
eliasyeme <eliasyeme@gmail.com>
2023-11-22 08:38:03 +0300

    python interview

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Island perimeter calculation"""
+
+
+def island_perimeter(grid: list[list[int]]) -> int:
+    """Calculate the perimeter an island
+
+    Args:
+        grid (list): matrix grid with 0 representing water 1 land
+    """
+    perimeter = 0
+
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                perimeter += 4
+
+                if col - 1 >= 0 and grid[row][col - 1] == 1:
+                    perimeter -= 1
+
+                if row - 1 >= 0 and grid[row - 1][col] == 1:
+                    perimeter -= 1
+
+                if col + 1 < len(grid[0]) and grid[row][col + 1] == 1:
+                    perimeter -= 1
+
+                if row + 1 < len(grid) and grid[row + 1][col] == 1:
+                    perimeter -= 1
+
+    return perimeter
